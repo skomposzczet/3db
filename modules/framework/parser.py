04f5854a53002a1edc62569baf53cdfdb1fc32c1
WaterIris <youiris13@gmail.com>
2024-10-26 23:58:15 +0200

    hmm

---
+++ modules/framework/parser.py
@@ -0,0 +1,52 @@
+import uuid
+import pandas as pd 
+import numpy as np
+#import timeit
+
+class Car:
+    def __init__(self, full_name: str, movie_title: str):
+        self.full_name = full_name
+        self.movie_title = movie_title
+
+    def get_db_record(self):
+        return {
+            "id": uuid.uuid4(),
+            "full_name": self.full_name,
+            "movie_title": self.movie_title,
+        }
+
+
+class Return_Car:
+    def __init__(self):
+        df = pd.read_csv(filepath_or_buffer="./data/Cars in Movies.csv",
+                         delimiter=";",
+                         index_col= None)
+        self.df = df[["Car Full Name","Movie Title"]]
+        self.s_ind = 0
+        self.e_ind = 0
+
+    def gimmie_amount(self,amount):
+        #start_time = timeit.default_timer()
+        self.e_ind+=amount
+        if self.s_ind == self.df.shape[0]:
+            return np.array([],dtype=Car)
+
+        if self.e_ind > self.df.shape[0]:
+            self.e_ind = self.df.shape[0]
+            amount = self.df.shape[0] - self.s_ind
+        else :
+            pass
+
+        print(f"indexes {self.s_ind} : {self.e_ind-1}")
+        dt = np.empty(shape = amount,dtype=Car)
+        d = self.df.iloc[self.s_ind:self.e_ind,:]
+        n = 0
+        for index, row in d.iterrows():
+            sample = Car(full_name = row["Car Full Name"],movie_title=row["Movie Title"]) 
+            dt[n] = sample
+            n+=1
+        
+        self.s_ind = self.e_ind
+        #print(f"Time taken : {timeit.default_timer() - start_time:<.1f}s")
+        return dt
+
