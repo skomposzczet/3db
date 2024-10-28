2db682f48211a50b5bec9427eed47979cf2b4020
WaterIris <youiris13@gmail.com>
2024-10-29 00:09:53 +0100

    resolved remaining issues

--- modules/framework/parser.py
+++ modules/framework/parser.py
@@ -5,35 +5,29 @@ from modules.db.model.car import Car
 
 class Return_Car:
     def __init__(self):
-        df = pd.read_csv(filepath_or_buffer="./data/CarsInMovies.csv",
+        data_frame = pd.read_csv(filepath_or_buffer="./data/CarsInMovies.csv",
                          delimiter=";",
                          index_col= None)
-        self.df = df[["Car Full Name","Movie Title"]]
-        self.s_ind = 0
-        self.e_ind = 0
+        self.data_frame = data_frame[["Car Full Name","Movie Title"]]
+        self.start_ind = 0
 
     def gimmie_amount(self,amount):
-        #start_time = timeit.default_timer()
-        self.e_ind+=amount
-        if self.s_ind == self.df.shape[0]:
+        end_ind = self.start_ind + amount
+
+        if self.start_ind == self.data_frame.shape[0]:
             return np.array([],dtype=Car)
 
-        if self.e_ind > self.df.shape[0]:
-            self.e_ind = self.df.shape[0]
-            amount = self.df.shape[0] - self.s_ind
-        else :
-            pass
+        if end_ind > self.data_frame.shape[0]:
+            end_ind = self.data_frame.shape[0]
+            amount = self.data_frame.shape[0] - self.start_ind
 
-        print(f"indexes {self.s_ind} : {self.e_ind-1}")
-        dt = np.empty(shape = amount,dtype=Car)
-        d = self.df.iloc[self.s_ind:self.e_ind,:]
+        dt_list = np.empty(shape = amount,dtype=Car)
+        d_slice = self.data_frame.iloc[self.start_ind:end_ind,:]
         n = 0
-        for index, row in d.iterrows():
-            sample = Car(full_name = row["Car Full Name"],movie_title=row["Movie Title"]) 
-            dt[n] = sample
+        for _, row in d_slice.iterrows():
+            sample = Car(full_name = row["Car Full Name"], movie_title = row["Movie Title"]) 
+            dt_list[n] = sample
             n+=1
         
-        self.s_ind = self.e_ind
-        #print(f"Time taken : {timeit.default_timer() - start_time:<.1f}s")
-        return dt
-
+        self.start_ind = end_ind
+        return dt_list
