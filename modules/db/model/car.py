809f64b399da397c573b9c86fb0aec92f072943a
CRD201 <jakub.koch2001@gmail.com>
2024-10-29 21:36:01 +0100

    fix postgres handler

--- modules/db/model/car.py
+++ modules/db/model/car.py
@@ -4,10 +4,11 @@ class Car:
     def __init__(self, full_name: str, movie_title: str):
         self.full_name = full_name
         self.movie_title = movie_title
+        self.uuid = uuid.uuid4()
 
     def get_db_record(self):
         return {
-            "id": uuid.uuid4(),
+            "id": self.uuid,
             "full_name": self.full_name,
             "movie_title": self.movie_title,
         }
