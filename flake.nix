c1cf2650175076544eee2a5a5f18301c7cd053aa
WaterIris <youiris13@gmail.com>
2024-11-09 11:29:07 +0100

    update nixos dependencies

--- flake.nix
+++ flake.nix
@@ -1,25 +1,22 @@
 {
-  description = "A Nix-flake-based Python development environment";
-
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
   outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          packages = with pkgs; [ python312 ] ++
-            (with pkgs.python312Packages; [
-              pip
-              venvShellHook
-            ]);
-        };
-      });
+  let 
+    pkgs = nixpkgs.legacyPackages."x86_64-linux"; 
+  in { 
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+      venvDir = ".venv";
+      packages = with pkgs; [
+        python312
+        python312Packages.venvShellHook
+        python312Packages.pip
+        python312Packages.numpy
+        python312Packages.pyqt6
+      ];
     };
+  };
 }
